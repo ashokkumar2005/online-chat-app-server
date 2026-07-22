7d874cb9e082ac73dad92f8ff714c4444bbaeec7
Ashokkumar <babajiashok8637@gmail.com>
2026-07-22 21:52:07 +0530

    update-soket.io

--- socket/socket.js
+++ socket/socket.js
@@ -1,32 +1,48 @@
 
-import {Server} from "socket.io";
+import {Server, Socket} from "socket.io";
 
  let io;
 
-export const initsocket = (server)=>{
+export const initsocket = (Server)=>{
 
    io = new Server(server, {
      cors:{
         origin: "http://localhost:5173",
-        method:["GET", "POST"]
+        methods:["GET", "POST"]
      }
    });
 
-   io.on("connection" , (soket)=>{
-      console.log("connected", socket.id);
+  io.on("connect",(Socket)=>{
 
-      socket.on("Hello" , (message)=>{
-        console.log("Client", message);
+      console.log("connected:", Socket.id);
 
-        socket.emit("Welcome" , " Welocome to Chat App");
+      Socket.on("join" , (UserId)=>{
+        users[UserId]=Socket.id;
+
+        console.log(users);
+
       })
+      
+      
+      Socket.on("disconnect", ()=>{
 
-      socket.on("disconnected" , (socket)=>{
-        console.log("User Disconnected" , socket.id);
-   })
-   })
+        for(const UserId in users ){
+          if( users[UserId] === Socket.id){
+            delete users[UserId];
+            break;
+          }
+        }
+        console.log(users);
+      })
+  })
+
 
    return io;
 };
 
 export const getIo = ()=> io;
+
+export const getReciverSocketId = (UserId)=>{
+   return users[UserId];
+
+}
